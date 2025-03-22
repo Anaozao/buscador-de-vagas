748e34a294fdce875b886cfb082b5d96391340e6
Tulio Paris <tutyparis@gmail.com>
2025-03-22 10:05:51 -0300

    linkedin página inteira

--- src/scraper/linkedin_scraper.py
+++ src/scraper/linkedin_scraper.py
@@ -5,9 +5,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 import json
+from dotenv import load_dotenv
+import os
 
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+load_dotenv()
 
 url = "https://www.linkedin.com/jobs/"
 options = Options()
@@ -31,17 +35,24 @@ def select_time_range(selected_time_range, time_options):
         select_time_range(new_option, time_options)
 
 
-def run():
-    firefox.get("https://www.linkedin.com/jobs/")
-    print("Necessário fazer login")
-    email = input("Digite seu e-mail: ")
-    password = getpass.getpass("Digite sua senha: ")
+def login(env_email, env_pass):
+    if env_email and env_pass:
+        email_input = firefox.find_element(By.ID, "session_key")
+        email_input.send_keys(env_email)
 
-    email_input = firefox.find_element(By.ID, "session_key")
-    email_input.send_keys(email)
+        password_input = firefox.find_element(By.ID, "session_password")
+        password_input.send_keys(env_pass)
 
-    password_input = firefox.find_element(By.ID, "session_password")
-    password_input.send_keys(password)
+    else:
+        print("Necessário fazer login")
+        email = input("Digite seu e-mail: ")
+        password = getpass.getpass("Digite sua senha: ")
+
+        email_input = firefox.find_element(By.ID, "session_key")
+        email_input.send_keys(email)
+
+        password_input = firefox.find_element(By.ID, "session_password")
+        password_input.send_keys(password)
 
     enter_btn = firefox.find_element(
         By.CLASS_NAME, "sign-in-form__submit-btn--full-width"
@@ -49,9 +60,11 @@ def run():
 
     enter_btn.send_keys(Keys.ENTER)
 
-    sleep(3)
 
-    search_input = firefox.find_element(By.CLASS_NAME, "jobs-search-box__text-input")
+def search(wait):
+    search_input = wait.until(
+        EC.presence_of_element_located((By.CLASS_NAME, "jobs-search-box__text-input"))
+    )
 
     query = input("Digite a vaga desejada: ")
 
@@ -78,12 +91,27 @@ def run():
     filter_btn = buttons_div.find_elements(By.TAG_NAME, "button")[1]
     filter_btn.click()
 
+
+def scrape_jobs():
     jobs_cards = firefox.find_elements(By.CLASS_NAME, "job-card-container__link")
+
+    while True:
+        last_job = jobs_cards[-1]
+        last_job.location_once_scrolled_into_view
+        sleep(2)
+
+        new_jobs_cards = firefox.find_elements(
+            By.CLASS_NAME, "job-card-container__link"
+        )
+
+        if len(new_jobs_cards) == len(jobs_cards):
+            break
+
+        jobs_cards = new_jobs_cards
 
     jobs = []
 
     for job in jobs_cards:
-        # base_url = "https://www.linkedin.com"
 
         job_name = job.get_attribute("aria-label")
         job_link = job.get_attribute("href")
@@ -94,5 +122,18 @@ def run():
 
     with open("src/jobs/vagas.json", mode="w", encoding="utf-8") as file:
         json.dump(jobs, file, ensure_ascii=False, indent=4)
+
+
+def run():
+    firefox.get("https://www.linkedin.com/jobs/")
+    wait = WebDriverWait(firefox, 10)
+    env_email = os.getenv("LINKEDIN_EMAIL")
+    env_pass = os.getenv("LINKEDIN_PASSWORD")
+
+    login(env_email, env_pass)
+
+    search(wait)
+
+    scrape_jobs()
 
     # firefox.quit()
